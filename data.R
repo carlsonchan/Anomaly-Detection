4fd614cbad71074e2649e470f7bc96d3725a7408
Zeeshaan <Zeeshaan@zeeshans-MBP.hitronhub.home>
2018-03-18 03:06:53 -0700

    Updated Point Anomaly Function

--- data.R
+++ data.R
@@ -7,7 +7,6 @@ class(DataDf)
 library(ggplot2)
 head(DataDf)
 set.seed(150)
-
 setwd("/Users/zeeshan/Desktop/cmpt-318-group4")
 getwd()
 
@@ -55,10 +54,33 @@ findPoint<-function(a,value){
       print("Normal")
     }
   }
- 
 }
+
+find_point<-function(train,test,col){
+  find_min<-min(train[,col],na.rm=TRUE)
+  find_max<-max(train[,col],na.rm=TRUE)
+  x<-c(test[,col])
+  for(i in 1:length(x)){
+    if(is.na(x[i])){
+      test$set[i]="NOISE"
+    }
+    else if (x[i]>find_max){ # If the value is greater than the max of the training set we put it as an anomaly
+      test$set[i]="Anomaly"
+    }
+    else if (x[i]<find_min)
+    {
+      test$set[i]="Anomaly"
+    }
+    else
+    {
+      test$set[i]="Normal"
+    }
+  }
+  return(test)
+}
+
 require(zoo)
-#Printing Friday evening 
+#Adding Columns to Dataset to help with the Data
 DataDf <- read.table("train.txt", header = T, sep = ",")
 DataDf$day <- weekdays(as.Date(DataDf$Date,'%d/%m/%Y'))
 DataDf$Month<-returnMonth(as.Date(DataDf$Date))
@@ -80,46 +102,20 @@ p_a<-zoo(c(summer$Global_active_power))
 x<-rollapply(p_a,width=15,by=14,FUN=mean,align="left")
 update<-data.frame(x)
 
-#Finding Max and Min of Columns in Dataset to find anomalies ( Max and Min from Train to find anomalies in the test dataset)
-find_min<-min(train$Global_active_power)
-find_max<-max(train$Global_active_power)
-x<-c(test$Global_active_power)
-for(i in 1:length(x)){
-  
-  if (x[i]>find_max){ # If the value is greater than the max of the training set we put it as an anomaly
-    test$set[i]="Anomaly"
-    
-  }
-  else if (x[i]<find_min)
-  {
-    test$set[i]="Anomaly"
-  }
-  else
-  {
-    test$set[i]="Normal"
-  }
-}
+#Finding Anomalies using Max and Min of Training Set 
+b<-find_point(train,test,'Voltage')
 
 #Writing the data frame to a file
 write.table(x,"x.txt",sep="\t",row.names=TRUE)
 write.table(summer,"summer.txt",sep="\t",row.names=TRUE)
-write.table(test,"testanomaly.txt",sep="\t",row.names=TRUE)
-
-
-newdata <- DataDf[ which(DataDf$day=='Friday' & DataDf$Hour>=16), ]
-class(newdata)
-write.table(newdata,"Update.txt",sep="\t",row.names=TRUE)
-#train <- newdata[, c(1,3)]
-print(newdata)
-#colnames(train) <- c("DateTime", "Global_active_power")
-#train$DateTime <- paste(train$Date, train$Time)
-
+write.table(b,"testanomaly.txt",sep="\t",row.names=TRUE)
+write.table(train,"updated_train.txt",sep="\t",row.names=TRUE)
 
 
 ############################################### Global Reactive power and active power 
 
 ggplot()+
-  layer(data = summer, mapping = aes(x=Global_reactive_power, y=Global_active_power), geom = "point",stat="identity", position = position_identity())
+  layer(data = summer, mapping = aes(x=Voltage, y=Global_active_power), geom = "point",stat="identity", position = position_identity())
 
 cor(DataDf$Global_reactive_power, DataDf$Global_active_power, use = "complete.obs", method = "pearson")
 v_mean <- mean(DataDf$Global_reactive_power, na.rm = TRUE)
@@ -132,48 +128,7 @@ v_sd2 <- sd(DataDf$Global_active_power, na.rm = TRUE)
 print(v_mean)
 print(v_median)
 print(v_sd)
-print("Global Active Results")
 print(v_mean2)
 print(v_median2)
 print(v_sd2)
-############################################### Time and Voltage
-
-ggplot()+
-  layer(data = DataDf, mapping = aes(x=Time, y=Voltage), geom = "point",stat="identity", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous()
-v_mean3 <- mean(DataDf$Voltage, na.rm = TRUE)
-v_median3 <- median(DataDf$Voltage, na.rm = TRUE)
-v_sd3 <- sd(DataDf$Voltage, na.rm = TRUE)
-print(v_mean3)
-print(v_median3)
-print(v_sd3)
-
-##########################Global Active Power and Global Intensity 
-
-ggplot()+
-  layer(data = DataDf, mapping = aes(x=Global_active_power, y=Global_intensity), geom = "point",stat="identity", position = position_identity())
-
-v_mean3 <- mean(DataDf$Global_intensity, na.rm = TRUE)
-v_median3 <- median(DataDf$Global_intensity, na.rm = TRUE)
-v_sd3 <- sd(DataDf$Global_intensity, na.rm = TRUE)
-print(v_mean3)
-print(v_median3)
-print(v_sd3)
-cor(DataDf$Global_active_power, DataDf$Global_intensity, use = "complete.obs", method = "pearson")
-
-################## Gloabl Reactive Power and Global Intensity 
-ggplot()+
-  layer(data = DataDf, mapping = aes(x=Global_reactive_power, y=Global_intensity), geom = "point",stat="identity", position = position_identity()) +
-  coord_cartesian() +
-  scale_x_discrete() +
-  scale_y_continuous()
-cor(DataDf$Global_reactive_power, DataDf$Global_intensity, use = "complete.obs", method = "pearson")
-v_mean3 <- mean(DataDf$Global_intensity, na.rm = TRUE)
-v_median3 <- median(DataDf$Global_intensity, na.rm = TRUE)
-v_sd3 <- sd(DataDf$Global_intensity, na.rm = TRUE)
-print(v_mean3)
-print(v_median3)
-print(v_sd3)
 
