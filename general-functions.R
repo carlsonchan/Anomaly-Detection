9351aee9f93e1087f910a868bc03c6468aab4a00
Unknown <carlsonc@sfu.ca>
2018-03-29 20:13:50 -0700

    updated boxplot and created function

--- general-functions.R
+++ general-functions.R
@@ -28,3 +28,26 @@ returnDay<-function(a){
   new<-as.numeric(x)
   return (new)
 }
+
+returnMean<-function(df,column){
+  # Extract 3 column we need
+  newDataDF<-df[,c('Date','Time',column)]
+  
+  # Remove values that are NA
+  cleanData<- newDataDF[complete.cases(newDataDF),]
+  
+  # Aggregate the mean per day
+  meanData <-data.frame(aggregate(cleanData[,3], list(cleanData$Date),mean))
+  # Reformat date
+  meanData <- meanData[order(as.Date(meanData$Group.1, format="%d/%m/%Y")),]
+  # Rename column
+  colnames(meanData)[1] <- "Date"
+  colnames(meanData)[2] <- "Average"
+  
+  meanData$Month<-returnMonth(as.Date(meanData$Date))
+  meanData$year<-returnYear(meanData$Date)
+  
+  #Sorting months
+  meanData$month <- factor(strftime(meanData$Date,"%b"),levels=month.abb)
+  return (meanData)
+}
