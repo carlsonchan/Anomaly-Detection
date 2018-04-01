19a4a6fc763c5a794f953a4e310def319e98a8e9
Unknown <carlsonc@sfu.ca>
2018-03-31 19:11:48 -0700

    Added general data exploration code and correlation

--- general-exploration.R
+++ general-exploration.R
@@ -11,28 +11,70 @@ source("general-functions.R")
 DataDf <- read.table("train.txt", header = T, sep = ",")
 #DataDf$day <- weekdays(as.Date(DataDf$Date,'%d/%m/%Y'))
 #DataDf$Month<-returnMonth(as.Date(DataDf$Date))
-#DataDf$Hour<-returnHour(DataDf$Time)
+DataDf$Hour<-returnHour(DataDf$Time)
 #DataDf$year<-returnYear(DataDf$Date)
 #DataDf$day<-returnDay(DataDf$Date)
 
 
 # Assume highest energy consumption is during after work hours
 afterWork<-DataDf[which(DataDf$Hour>=18 & DataDf$Hour<=23),] #06:00pm - 11:00pm
-
+summer[which(summer$year==2007),]$Average
 # Global Active Power
 meanGlobalActive<-returnMean(afterWork,'Global_active_power')
+
+mean_GA <- mean(meanGlobalActive$Average, na.rm = TRUE)
+median_GA <- median(meanGlobalActive$Average, na.rm = TRUE)
+sd_GA <- sd(meanGlobalActive$Average, na.rm = TRUE)
+max_GA <- max(meanGlobalActive$Average)
+min_GA <- min(meanGlobalActive$Average)
+
 ggplot(meanGlobalActive, aes(x=,month, y=Average)) + geom_boxplot() + facet_wrap(~year) +labs(y = "Average Global Active Power per day between 6PM-11PM")
+
+summer <- meanGlobalActive[which(meanGlobalActive$Month>=6 & meanGlobalActive$Month<=9),] # Extracting summer momnth June-Sept
+ggplot(summer, aes(x=,month, y=Average)) + geom_boxplot() + facet_wrap(~year) + labs(x = "Summer Months of 2007-2009") + labs(y = "Average Global Active Power")
+
+cor(summer[which(summer$year==2007),]$Average, summer[which(summer$year==2008),]$Average, use = "complete.obs", method = "pearson")
+cor(summer[which(summer$year==2007),]$Average, summer[which(summer$year==2009),]$Average, use = "complete.obs", method = "pearson")
+cor(summer[which(summer$year==2008),]$Average, summer[which(summer$year==2009),]$Average, use = "complete.obs", method = "pearson")
+
+cor(afterWork$Global_active_power, afterWork$Global_reactive_power, use = "complete.obs", method = "pearson")
+cor(afterWork$Global_active_power, afterWork$Global_intensity, use = "complete.obs", method = "pearson")
+cor(afterWork$Global_active_power, afterWork$Voltage, use = "complete.obs", method = "pearson")
+cor(afterWork$Global_reactive_power, afterWork$Global_intensity, use = "complete.obs", method = "pearson")
+cor(afterWork$Global_reactive_power, afterWork$Voltage, use = "complete.obs", method = "pearson")
+cor(afterWork$Global_intensity, afterWork$Voltage, use = "complete.obs", method = "pearson")
 
 #Global reactive power
 meanGlobalReactive<-returnMean(afterWork, 'Global_reactive_power')
+
+mean_GR <- mean(meanGlobalReactive$Average, na.rm = TRUE)
+median_GR <- median(meanGlobalReactive$Average, na.rm = TRUE)
+sd_GR <- sd(meanGlobalReactive$Average, na.rm = TRUE)
+max_GR <- max(meanGlobalReactive$Average)
+min_GR <- min(meanGlobalReactive$Average)
+
 ggplot(meanGlobalReactive, aes(x=,month, y=Average)) + geom_boxplot() + facet_wrap(~year) + labs(y = "Average Global Reactive Power per day between 6PM-11PM")
 
 #Voltage
 meanVoltage<-returnMean(afterWork, 'Voltage')
+
+mean_GV <- mean(meanVoltage$Average, na.rm = TRUE)
+median_GV <- median(meanVoltage$Average, na.rm = TRUE)
+sd_GV <- sd(meanVoltage$Average, na.rm = TRUE)
+max_GV <- max(meanVoltage$Average)
+min_GV <- min(meanVoltage$Average)
+
 ggplot(meanVoltage, aes(x=,month, y=Average)) + geom_boxplot() + facet_wrap(~year) + labs(y = "Average Voltage per day between 6PM-11PM")
 
 #Global Intensity
 meanGlobalIntensity<-returnMean(afterWork, 'Global_intensity')
+
+mean_GI <- mean(meanGlobalIntensity$Average, na.rm = TRUE)
+median_GI <- median(meanGlobalIntensity$Average, na.rm = TRUE)
+sd_GI <- sd(meanGlobalIntensity$Average, na.rm = TRUE)
+max_GI <- max(meanGlobalIntensity$Average)
+min_GI <- min(meanGlobalIntensity$Average)
+
 ggplot(meanGlobalIntensity, aes(x=,month, y=Average)) + geom_boxplot() + facet_wrap(~year) +labs(y = "Average Global Intensity per day between 6PM-11PM")
 
 #Functions for Data Exploration
